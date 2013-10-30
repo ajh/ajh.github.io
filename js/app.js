87e799546a3ed7623ae07d584b06720330f2c8fd
Andrew Hartford <andrew.hartford@doxo.com>
2013-10-30 12:09:56 -0400

    Remove old comment

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ var tableOfContents = {
 
     $('section header').append("<h4 class='toc'>Contents:</h4><ul id='toc'></ul>");
 
-    // TODO: skip the first h1
     headers.each(function() {
       var text = $(this).text();
       var name = text.replace(/[^a-zA-z_]/g, '')
